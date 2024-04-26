529e86ccd49bc3319d128e980e23130f55c3f27a
Rama Devi <maheshwaram.rama@gmail.com>
2024-04-26 23:50:44 +0530

    Commit changes

---
+++ src/Lab_18042024/RelationShip_Operators.java
@@ -0,0 +1,21 @@
+package Lab_18042024;
+
+public class RelationShip_Operators {
+    public static void main(String[] args) {
+        // Relationship Operators are >, <, >=,<=, ==,!
+        int a= 10;
+        int b = +23;
+        //Output is 23, because its a unary operator +/- number.
+        int c = -56;
+        //Output i -56
+        System.out.println(a+b);
+        System.out.println(a>12);
+        System.out.println(a<12);
+        System.out.println(a>=10);
+        System.out.println(a<=10);
+        System.out.println(a==10);
+        boolean result = a>b;
+        System.out.println(result);
+
+    }
+}
